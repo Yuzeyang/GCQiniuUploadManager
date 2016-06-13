96aa596fdda45ea2a29390b5577046ab111fbccb
yuzeyang <yuzeyang@qima-inc.com>
2016-06-14 00:17:27 +0800

    add files

---
+++ QiniuUploadManager/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QiniuUploadManager
+//
+//  Created by 宫城 on 16/6/13.
+//  Copyright © 2016年 宫城. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QiniuUploadManager/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QiniuUploadManager
+//
+//  Created by 宫城 on 16/6/13.
+//  Copyright © 2016年 宫城. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
